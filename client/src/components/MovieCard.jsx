301d61c6f2aacdbaadcd4b42fa754eb486a9c68e
Hius-234 <hieudo.042324@gmail.com>
2025-11-01 22:20:37 +0700

    Add MovieCard and TrailersSection components; update FeaturedSection to include MovieCard and improve styling

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,18 +1,20 @@
+import { StarIcon } from 'lucide-react';
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import timeFormat from '../lib/timeFormat';
 
 const MovieCard = ({ movie }) => {
     const navigate = useNavigate();
   return (
-    <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2x1 hover:-translate-y-1 transition duration-300 w-66'>
+    <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-64'>
 
       <img onClick={ ()=> {navigate(`/movies/${movie._id}`); scrollTo(0, 0)}}
-      src={movie.backdrop_path} alt="" className='rounded-1g h-52 w-full object-cover object-bottom-right cursor-pointer' />
+      src={movie.backdrop_path} alt="" className='rounded-lg h-52 w-full object-cover object-bottom-right cursor-pointer' />
 
       <p className='font-semibold mt-2 truncate'>{movie.title}</p>
 
       <p className='text-sm text-gray-400 mt-2'>
-        {new Date(movie.release_date).getFullYear()}. {movie.genres.slice(0, 2).map(genre => genre.name).join(" | ")}. {movie.runtime}IT
+        {new Date(movie.release_date).getFullYear()} • {movie.genres.slice(0, 2).map(genre => genre.name).join(" | ")} • {timeFormat(movie.runtime)}    
       </p>
 
       <div className='flex items-center justify-between mt-4 pb-3'>
