792524d2300639b000a4a7c5a3c650ec03dae8aa
Hius-234 <hieudo.042324@gmail.com>
2025-11-04 09:34:58 +0700

    update

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { connect } from 'mongoose';
-import connectDB from './configs/db';
+import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest.js"
 
 const app = express();
 const port = 3000;
@@ -17,5 +19,6 @@ app.use(clerkMiddleware())
 
 // API Routes
 app.get('/', (req, res)=> res.send('Server is Live!'))
+app.use( '/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(port, ()=> console.log(`Server listening at http://localhost:${port}`));
