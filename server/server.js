f7573e991b66e06cfb5570127fd86b12cdc62b81
Hius-234 <hieudo.042324@gmail.com>
2025-11-04 08:39:40 +0700

    update

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { connect } from 'mongoose';
+import connectDB from './configs/db';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const port = 3000;
 
+await connectDB()
+
 // Middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 // API Routes
 app.get('/', (req, res)=> res.send('Server is Live!'))
